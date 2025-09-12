5c6f99e7a3f5d9a2bd0f037f3110f340fc18d2f4
muhammad uzair <abuzada735@gmail.com>
2025-09-13 03:50:56 +0500

    all done

---
+++ react assaignment/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Header from './Components/Header/Header.jsx'
+import Cards from './Components/Cards.jsx'
+import   "./App.css"
+import Footer from './Components/Footer.jsx'
+function App() {
+  return (
+    <>
+      <Header />
+      <Cards />
+      <Footer />
+    </>
+  )
+}
+
+export default App
